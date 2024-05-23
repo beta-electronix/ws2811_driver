9138b2ba4cb1288ae30208e95fd03717f8251b09
beta-electronix <pengchenglv2018@gmail.com>
2024-05-23 13:00:32 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
-basic.forever(function () {
-	
-})
+namespace beta-electronix_WS2811 {
+    //% shim=sendDataAsm
+    export function sendBuffer(buf: Buffer, pin: DigitalPin) {
+    }
+
+    //% shim=setMode
+    export function setBufferMode(pin: DigitalPin, mode: number) {
+
+    }
+}
+
